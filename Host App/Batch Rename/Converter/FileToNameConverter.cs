fed29e55364c55edc39d3160a19ec08396a271bb
Tung Nguyen <ncnt257@gmail.com>
2021-12-20 20:23:45 +0700

    update UI for gridspliter

---
+++ Host App/Batch Rename/Converter/FileToNameConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Batch_Rename.Converter
+{
+    class FileToNameConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string fullName = (string)(value);
+            return System.IO.Path.GetFileName(fullName);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Host App/Batch Rename/MainWindow.xaml.cs
+++ Host App/Batch Rename/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Path = System.IO.Path;
 
 namespace Batch_Rename
 {
@@ -32,122 +35,155 @@ namespace Batch_Rename
 
         private void FilesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
-        }
-
-        //Browse files
-        private void WindowsExplorerButton_Click(object sender, RoutedEventArgs e)
-        {
-            Microsoft.Win32.OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Multiselect = true;
-
-            //filter browser
-            openFileDialog.Filter = "ALL Files|*.*|TEXT FILE | *.txt|PDF FILE |*.pdf|Folders|\n";
-
-            bool? response = openFileDialog.ShowDialog();
-
-            if (response == true)
+            foreach (var filepath in filepaths)
             {
-                int fileCount = openFileDialog.FileNames.Count();
-                for (int i = 0; i < fileCount; i++)
-                {
-                    //check if duplicate then abort that choosen file
-                    bool flag = true;
-                    for (int j = 0; j < filepaths.Count(); j++)
-                    {
-                        if (filepaths[j].Root == openFileDialog.FileNames[i])
-                        {
-                            flag = false;
-                            break;
-                        }
-                    }
-
-                    //add file vao array
-                    CFile temp=new CFile();
-                    temp.Root = openFileDialog.FileNames[i];
-                    if (flag) filepaths.Add(temp);
-                    //if (flag) filepaths.Add(openFileDialog.FileNames[i]);
-                    //MessageBox.Show(openFileDialog.FileNames[i])
-                }
+                filepath.PreviewName += "la";
             }
         }
+
+
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             FilesListView.ItemsSource = filepaths;
         }
 
-        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        private void ApplyButton_OnClick(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void ListViewItem_Selected(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ApplyRulesButton_Click(object sender, RoutedEventArgs e)
-        {
-            //Duyệt tất cả các file đã chọn ở listview
-            //Cần 1 biến cho biết copy hay chỉ đổi tên nữa
-            for (int i = 0; i < filepaths.Count(); i++)
+            foreach (var filepath in filepaths)
             {
-                if (filepaths[i].isChecked) //MessageBox.Show(filepaths[i].Root);
+                filepath.PreviewName+="123";
+            }
+            foreach (var filepath in filepaths)
+            {
+                filepath.Rename(CopyToTextBlock.Text!=""? CopyToTextBlock.Text:filepath.Path);
+            }
+        }
+
+
+        private void RemoveFileButtonMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            filepaths.RemoveAt(FilesListView.SelectedIndex);
+        }
+
+        //Browse files
+        private void FileExplorerButton_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new CommonOpenFileDialog();
+            fileDialog.Multiselect = true;
+
+            //filter browser
+            if (fileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                foreach (var fileName in fileDialog.FileNames)
                 {
-                    //Đổi tên
-                    //Nếu chỉ đổi chứ kp copy thì...
-                    string newName = $"{filepaths[i].getDir()}{filepaths[i].PreviewName}";
-                    System.IO.File.Move(filepaths[i].Root, newName);
-                    //Cập nhật lại cái dường dẫn tuyệt đối
-                    filepaths[i].Root = newName;
-                    //Bỏ check đi, cái này tùy
-                    filepaths[i].isChecked = false;
 
+                    //kiểm tra xem file đã tồn tại trong list chưa
+                    var fn = filepaths.SingleOrDefault(f => Path.GetFullPath(f.FullName) == Path.GetFullPath(fileName));//chuyển hết về 1 dạng ful path name để so sánh chuỗi
+                    if (fn == null)
+                    {
+                        var newFile = new CFile()
+                        {
+                            IsChecked = true,
+                            Path = Path.GetDirectoryName(fileName),
+                            Name = Path.GetFileName(fileName),
+                            PreviewName = Path.GetFileName(fileName),
+                            IsFile = true // folder
 
-                    //Nếu copy thì...
-
-
-                    //Cuối cùng set PreviewName về rỗng
-                    filepaths[i].PreviewName = "";
+                        };
+                        filepaths.Add(newFile);
+                    }
                 }
             }
-            ////Lấy các file được chọn (cố gắng lấy index) bỏ vào list tạm
-            //int selectedFilesCount = FilesListView.SelectedItems.Count;
-            //List<CFile> selectedFiles = new List<CFile>();
-            //for (int i = 0; i < selectedFilesCount; i++)
-            //    selectedFiles.Add((CFile)(FilesListView.SelectedItems[i]));
 
-            ////Apply rules vào 
-            ////check copy hay rename để lấy root mới
 
-            //for (int i = 0; i < selectedFilesCount; i++)
-            //{
-            //    string newName = @"D:\updatedddd.txt";
-            //    //System.IO.File.Move(selectedFiles[i].Root, newName);
-
-            //    filepaths[0].PreviewName = newName;
-            //    MessageBox.Show(filepaths[i].Root);
-            //}
         }
 
-        private void PreviewChange_Click(object sender, RoutedEventArgs e)
+
+
+        private void FolderExplorerButton_Click(object sender, RoutedEventArgs e)
         {
-            //Duyệt tất cả các file đã chọn ở listview
-            for (int i = 0; i < filepaths.Count(); i++)
+
+            CommonOpenFileDialog folderDialog = new CommonOpenFileDialog();
+            folderDialog.IsFolderPicker = true;
+            folderDialog.Multiselect = true;
+
+            if (folderDialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
-                if (filepaths[i].isChecked) 
+                foreach (var folderName in folderDialog.FileNames)
                 {
-                    //Áp luật vào rồi gán vào ReviewName, lấy tên hiện tại bằng .getCurrentName()
-                    //Chỗ này gán để test thôi
-                    filepaths[i].PreviewName = $"{i}_{filepaths[i].getCurrentName()}";
+
+                    //kiểm tra xem file đã tồn tại trong list chưa
+                    var fn = filepaths.SingleOrDefault(f => Path.GetFullPath(f.FullName) == Path.GetFullPath(folderName));//chuyển hết về 1 dạng ful path name để so sánh chuỗi
+                    if (fn == null)
+                    {
+                        var newFile = new CFile()
+                        {
+                            IsChecked = true,
+                            Path = Path.GetDirectoryName(folderName),
+                            Name = Path.GetFileName(folderName),
+                            PreviewName = Path.GetFileName(folderName),
+                            IsFile=false // folder
+                        };
+                        filepaths.Add(newFile);
+                    }
                 }
             }
+
+
+
+
+
+            //Microsoft.Win32.OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Multiselect = true;
+
+                ////filter browser
+                //openFileDialog.Filter = "ALL Files|*.*|TEXT FILE | *.txt|PDF FILE |*.pdf|Folders|\n";
+
+                //openFileDialog.ValidateNames = false;
+                //openFileDialog.CheckFileExists = false;
+                //openFileDialog.CheckPathExists = true;
+
+
+
+                //// Always default to Folder Selection.
+                //openFileDialog.FileName = System.IO.Path.GetFileName(openFileDialog.FileName);
+                //bool? response = openFileDialog.ShowDialog();
+
+                //if (response == true)
+                //{
+                //    int fileCount = openFileDialog.FileNames.Count();
+                //    for (int i = 0; i < fileCount; i++)
+                //    {
+                //        //check if duplicate then abort that choosen file
+                //        bool flag = true;
+                //        for (int j = 0; j < filepaths.Count(); j++)
+                //        {
+                //            if (filepaths[j].FullName == openFileDialog.FileNames[i])
+                //            {
+                //                flag = false;
+                //                break;
+                //            }
+                //        }
+                //        if (flag)
+                //        {
+                //            //add file vao array
+                //            CFile temp = new CFile
+                //            {
+                //                //GetFileName trả về tên file hoặc tên folder lun
+                //                Name = System.IO.Path.GetFileName(openFileDialog.FileNames[i]),
+                //                PreviewName = System.IO.Path.GetFileName(openFileDialog.FileNames[i]),
+                //                //GetDirectoryName trả về tên đường dẫn đén file hoặc folder đó
+                //                Path = System.IO.Path.GetDirectoryName(openFileDialog.FileNames[i]),
+                //                IsChecked = true
+
+                //            };
+                //            filepaths.Add(temp);
+                //        }
+
+                //    }
+                //}
         }
     }
 }
