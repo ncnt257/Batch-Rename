bd963c384dd26228f55111f05a9d2d6f31070af0
Tung Nguyen <ncnt257@gmail.com>
2021-12-24 14:58:26 +0700

    add add counter rule

---
+++ Rename Rules/AddCounterRule/AddCounterRule/AddCounterArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contract;
+
+namespace AddCounterRule
+{
+    class AddCounterArgs:StringArgs
+    {
+        public int? Start { get; set; }
+        public int? Step { get; set; }
+        public int? Digit { get; set; }
+    }
+}
