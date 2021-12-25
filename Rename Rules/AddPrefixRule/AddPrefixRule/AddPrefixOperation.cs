726d7d082559af4f0514f6ade926c75b9e8fbe4b
trandactoan <73386087+trandactoan@users.noreply.github.com>
2021-12-25 15:27:36 +0700

    Add Prefix Rule
    -Add "AddPrefixRule" solution to RenameRules
    -Add "AddPrefixOperation" class
    -Add "AddPrefixUC" usercontrol
    -Add "AddPrefixArg" class
    -Add "AddPrefix.dll" to the debug file of "Batch-Rename" project

---
+++ Rename Rules/AddPrefixRule/AddPrefixRule/AddPrefixOperation.cs
@@ -0,0 +1,95 @@
+﻿using Contract;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+namespace AddPrefixRule
+{
+    public class AddPrefixOperation : IStringOperation
+    {
+        public StringArgs Args { get; set; }
+        public string Name => "Add Prefix";
+
+        public string Description
+        {
+            get
+            {
+                var args = Args as AddPrefixArgs;
+                return $"Add prefix of {args.Prefix}";
+            }
+        }
+
+        public UserControl ConfigUC { get; set; }
+        public bool IsChecked { get; set; }
+
+        public event IStringOperation.Trigger PreviewTriggerEvent;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public bool IsValidParams => true;
+
+
+        public AddPrefixOperation()//đừng quên cài đặt constructor
+        {
+            Args = new AddPrefixArgs();
+            ConfigUC = new AddPrefixRuleUC(this);
+            ConfigUC.DataContext = this.Args;
+        }
+        public IStringOperation Clone()
+        {
+            var oldArgs = Args as AddPrefixArgs;
+            var clone = new AddPrefixOperation()
+            {
+                Args = new AddPrefixArgs()
+                {
+                    Prefix = oldArgs.Prefix
+                },
+                IsChecked = true,
+                PreviewTriggerEvent = this.PreviewTriggerEvent
+            };
+            return clone;
+        }
+        public void ResetRule()
+        {
+        }
+
+        public string Operate(string origin, bool isFile)
+        {
+            var args = Args as AddPrefixArgs;
+            string prefix = args.Prefix;
+            if (!isFile)
+            {
+                return prefix + origin;
+            }
+            Regex ext = new Regex("[.]\\w{2,}$");
+            string extend = ext.Match(origin).ToString();
+            int idx = origin.IndexOf(extend);
+            string newName = prefix + origin.Substring(0, idx) + extend;
+            return newName;
+        }
+
+        public void CreateFromRaw(RawRule input)
+        {
+            IsChecked = input.IsChecked;
+            Args = new AddPrefixArgs()
+            {
+                Prefix = input.AgrList[0]
+            };
+            ConfigUC.DataContext = this.Args;
+        }
+
+        public List<string> GetStringAgrs()
+        {
+            List<string> resultList = new List<string>();
+            var args = Args as AddPrefixArgs;
+            resultList.Add(args.Prefix);
+            return resultList;
+        }
+
+        public void DescriptionChangedNotify()
+        {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs("Description"));
+            PreviewTriggerEvent?.Invoke();
+        }
+    }
+}
---
+++ Rename Rules/AddPrefixRule/AddPrefixRule/AddPrefixRuleUC.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AddPrefixRule
+{
+    /// <summary>
+    /// Interaction logic for AddPrefixRuleUC.xaml
+    /// </summary>
+    public partial class AddPrefixRuleUC : UserControl
+    {
+        private readonly AddPrefixOperation _operation;
+
+        public AddPrefixRuleUC(AddPrefixOperation operation)
+        {
+            InitializeComponent();
+            _operation = operation;
+            var args = _operation.Args as AddPrefixArgs;
+            Param.Text = args.Prefix;
+        }
+        private void ConfirmBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var args = _operation.Args as AddPrefixArgs;
+            args.Prefix = Param.Text ?? "";
+            _operation.DescriptionChangedNotify();//notify thuộc tính description đã thay đổi
+        }
+    }
+}
